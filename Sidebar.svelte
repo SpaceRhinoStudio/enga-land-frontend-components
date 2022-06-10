c8a2651619797d1bcccadef71ecba6bfe6aa0ee6
clonomaer <clonomaer@gmail.com>
2022-06-10 21:21:05 +0200

    add sidebar

---
+++ Sidebar.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { config } from './configs'
+  import { Routes } from './configs/routes'
+  import Modal from './Modal.svelte'
+  import CloseIcon from './assets/icons/close-sidebar.svg'
+  import SvgIcon from './SVGIcon.svelte'
+  import { goto } from '$app/navigation'
+  import Logo from './assets/EngalandLogo.svg'
+  import WithScrollHint from './WithScrollHint.svelte'
+  import SidebarNavItem from './SidebarNavItem.svelte'
+
+  const routes = [
+    Routes.home,
+    Routes.dapp,
+    Routes.marketplace,
+    Routes.docs,
+    Routes.help,
+    Routes.tokenomics,
+    Routes.github,
+    Routes.community,
+    Routes.aboutUs,
+  ].map(x => config.routeConfig[x])
+
+  export let isOpen = false
+</script>
+
+<Modal animateWidth neverFullWidth className={{ bg: '!items-start' }} acceptExit bind:isOpen>
+  <nav
+    class="flex flex-col bg-primary-990 h-[var(--h-screen)] top-0 left-0 z-40 overflow-y-auto max-w-xs w-screen px-6">
+    <div class="space-y-7 sticky top-0 z-10">
+      <div class="flex justify-between items-center pt-8">
+        <div on:click={() => goto('/')}>
+          <SvgIcon Icon={Logo} width={'6.8rem'} height={'3.1rem'} dontFill />
+        </div>
+        <div on:click={() => (isOpen = false)}>
+          <SvgIcon Icon={CloseIcon} width={'1.375rem'} height={'1.375rem'} />
+        </div>
+      </div>
+      <hr class="h-px opacity-25 -mx-6 bg-primary-600" />
+    </div>
+    <WithScrollHint
+      className={{
+        container: 'grow -mx-6 pl-6',
+        wrapper: 'flex flex-col justify-between -mx-6 pl-6 pt-5',
+      }}>
+      <ul class="grow-0">
+        {#each routes as nav}
+          <SidebarNavItem item={nav} onRouteChange={() => (isOpen = false)} />
+        {/each}
+      </ul>
+    </WithScrollHint>
+    <div class="flex items-end sticky bottom-0 -mx-6 px-6 py-4">
+      <slot name="foot" />
+    </div>
+  </nav>
+</Modal>
---
+++ SidebarNavItem.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import type { RouteConfig } from './configs/routes'
+  import SvgIcon from './SVGIcon.svelte'
+  import ArrowDown from './assets/icons/arrow-down.svg'
+  import Link from './Link.svelte'
+  import { __$ } from './locales'
+  import { config } from './configs'
+
+  export let item: RouteConfig
+  export let className = ''
+  export let onRouteChange: () => void = () => {}
+
+  const subRoutes = item.subRoutes?.map(x => config.routeConfig[x]) ?? []
+</script>
+
+<Link
+  let:match
+  href={item.href}
+  on:click={() => onRouteChange()}
+  disabled={item.disabled}
+  element="div"
+  className={{ element: `relative text-lg py-3 transition-all ${className}` }}>
+  {#if match}
+    <div
+      class={`
+        flex items-center space-x-2
+        before:opacity-0
+        before:content-[""]
+        before:absolute
+        before:-left-10
+        before:w-6
+        before:h-6
+        before:shadow-[theme(colors.secondary.600)_0px_0px_20px_5px]
+        before:rounded-full
+        before:bg-secondary-500
+        before:transition-all
+        ${match?.partial ? `text-secondary-500 ${match.exact ? 'before:opacity-100' : ''}` : ''}
+      `}>
+      <SvgIcon Icon={item.icon} width={'1.375rem'} height={'1.375rem'} />
+      <span>{$__$?.nav[item.id]}</span>
+      {#if subRoutes.length}
+        <SvgIcon
+          Icon={ArrowDown}
+          width={'1.375rem'}
+          height={'1.375rem'}
+          className="ml-2 transition-all" />
+      {/if}
+    </div>
+    {#if subRoutes.length}
+      <div class="-ml-6 py-3 pl-12 left-0">
+        {#each subRoutes as sub}
+          <svelte:self item={sub} className="-ml-6 pl-6" {onRouteChange} />
+        {/each}
+      </div>
+    {/if}
+  {/if}
+</Link>
