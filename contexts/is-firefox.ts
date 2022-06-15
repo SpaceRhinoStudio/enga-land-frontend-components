35107c5f302d6db275310ecf38da29f18d412b37
clonomaer <clonomaer@gmail.com>
2022-06-15 15:43:25 +0200

    fix webkit backdrop detection

--- contexts/is-firefox.ts
+++ contexts/is-firefox.ts
@@ -2,6 +2,12 @@ import { Window$ } from '$lib/shared/observables/window'
 import { map, shareReplay } from 'rxjs'
 
 export const isFirefox$ = Window$.pipe(
-  map(() => !CSS.supports('backdrop-filter', 'blur(1px)')),
+  map(
+    () =>
+      !(
+        CSS.supports('backdrop-filter', 'blur(1px)') ||
+        CSS.supports('-webkit-backdrop-filter', 'blur(1px)')
+      ),
+  ),
   shareReplay(1),
 )
