52bf9e178b4c7d4dee1fdfb110665d9d5fd6e8a9
clonomaer <clonomaer@gmail.com>
2022-06-15 05:57:04 +0200

    add rotation capibility to random translate

--- RandomTranslate.svelte
+++ RandomTranslate.svelte
@@ -2,39 +2,37 @@
   import { onMount } from 'svelte'
   import { useWobble } from './helpers/wobble-svelte'
   import _ from 'lodash'
+  import { SpringConfig } from 'wobble'
 
-  function calcNew(prev: { x: number; y: number }, width: number, height: number) {
-    let randomX = (Math.random() - 0.5) * (width * 1.01)
-    let randomY = (Math.random() - 0.5) * (height * 1.01)
+  export let maxMove = 1
+  export let rotate = false
 
-    const newX =
-      Math.abs(prev.x + randomX) > width * 1.01
-        ? Math.sign(prev.x + randomX) * width * 0.5
-        : prev.x + randomX
-    const newY =
-      Math.abs(prev.y + randomY) > height * 1.01
-        ? Math.sign(prev.y + randomY) * height * 0.5
-        : prev.y + randomY
-
-    return {
-      x: newX,
-      y: newY,
-    }
+  function calcNew(prev: number, max: number) {
+    let rand = (Math.random() - 0.5) * (max * maxMove)
+    return Math.abs(prev + rand) > max * maxMove ? Math.sign(prev + rand) * max * 0.5 : prev + rand
   }
 
   let width: number
   let height: number
 
-  const [x, setX, springX] = useWobble({})
-  const [y, setY, springY] = useWobble({})
+  export let config: Partial<SpringConfig> = {}
+
+  const [x, setX, springX] = useWobble(config)
+  const [y, setY, springY] = useWobble(config)
+  const [r, setR, springR] = useWobble(config)
+
+  export { x, y, r }
 
   onMount(() => {
-    springX.current.updateConfig({ mass: (height + width) * 3 })
-    springY.current.updateConfig({ mass: (height + width) * 3 })
+    if (config.mass === undefined) {
+      springX.current.updateConfig({ mass: (height + width) * 3 })
+      springY.current.updateConfig({ mass: (height + width) * 3 })
+      springR.current.updateConfig({ mass: (height + width) * 20 })
+    }
     const newX = _.throttle(
       (v: number, x: number) => {
         if (v < 0.001) {
-          setX(calcNew({ x, y: 0 }, width, height).x)
+          setX(calcNew(x, width))
         }
       },
       1000,
@@ -43,7 +41,16 @@
     const newY = _.throttle(
       (v: number, y: number) => {
         if (v < 0.001) {
-          setY(calcNew({ x: 0, y }, width, height).y)
+          setY(calcNew(y, height))
+        }
+      },
+      1000,
+      { leading: true, trailing: true },
+    )
+    const newR = _.throttle(
+      (v: number, r: number) => {
+        if (v < 0.001) {
+          setR(calcNew(r, 360))
         }
       },
       1000,
@@ -51,12 +58,17 @@
     )
     newX(0, 0)
     newY(0, 0)
+    rotate && newR(0, 0)
     springX.current.onUpdate(x => {
       newX(x.currentVelocity, x.currentValue)
     })
     springY.current.onUpdate(y => {
       newY(y.currentVelocity, y.currentValue)
     })
+    rotate &&
+      springR.current.onUpdate(r => {
+        newR(r.currentVelocity, r.currentValue)
+      })
   })
 
   export let className: string = ''
@@ -65,7 +77,7 @@
 <div
   bind:clientWidth={width}
   bind:clientHeight={height}
-  style="transform: translate({$x}px, {$y}px);"
+  style="transform: translate({$x}px, {$y}px) rotate({$r}deg);"
   class="will-change-transform {className}">
-  <slot x={10} y={10} />
+  <slot x={$x} y={$y} r={$r} />
 </div>
