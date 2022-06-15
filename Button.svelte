157c9949a5317e6a9a4fa0346c81280c8e5eaa14
clonomaer <clonomaer@gmail.com>
2022-06-15 13:49:53 +0200

    nit cleanup

--- Button.svelte
+++ Button.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import cn from 'classnames'
+
   import { canHover$ } from './helpers/media-queries'
 
   import LoadingOverlay from './LoadingOverlay.svelte'
@@ -13,41 +15,31 @@
 </script>
 
 <button
-  class={`
-    ${
-      $canHover$ && !(isLoading || isJobLoading || disabled)
-        ? 'hover:scale-105 transition-all'
-        : 'transition-colors'
-    }
-    relative
-    border
-    duration-500
-    disabled:border-gray-500
-    disabled:bg-gray-800
-    disabled:bg-opacity-30
-    disabled:text-gray-500
-    hover:border-gray-400
-    hover:text-text-hover
-    disabled:cursor-not-allowed
-    ${
-      active
-        ? 'border-none bg-secondary-800 hover:bg-secondary-600 hover:bg-opacity-50 active:bg-secondary-800 text-secondary-400 bg-opacity-50'
-        : danger
-        ? 'border-none bg-blood hover:bg-blood bg-opacity-70 active:bg-blood text-rose-200'
-        : 'border-primary-600 active:bg-primary-600'
-    }
-    ${
-      isLoading || isJobLoading
-        ? 'cursor-wait disabled:cursor-wait text-transparent hover:text-transparent disabled:text-transparent'
-        : ''
-    }
-    ${
-      secondary
-        ? 'py-1 px-2 rounded-xl text-xs bg-primary-600 leading-none'
-        : 'py-2 px-4 rounded-lg'
-    }
-    ${className}
-`}
+  class={cn(
+    $canHover$ && !(isLoading || isJobLoading || disabled)
+      ? 'hover:scale-105 transition-all'
+      : 'transition-colors',
+    'relative',
+    'border',
+    'duration-500',
+    'disabled:border-gray-500',
+    'disabled:bg-gray-800',
+    'disabled:bg-opacity-30',
+    'disabled:text-gray-500',
+    'hover:border-gray-400',
+    'hover:text-text-hover',
+    'disabled:cursor-not-allowed',
+    active
+      ? 'border-none bg-secondary-800 hover:bg-secondary-600 hover:bg-opacity-50 active:bg-secondary-800 text-secondary-400 bg-opacity-50'
+      : danger
+      ? 'border-none bg-blood hover:bg-blood bg-opacity-70 active:bg-blood text-rose-200'
+      : 'border-primary-600 active:bg-primary-600',
+    isLoading ||
+      (isJobLoading &&
+        'cursor-wait disabled:cursor-wait text-transparent hover:text-transparent disabled:text-transparent'),
+    secondary ? 'py-1 px-2 rounded-xl text-xs bg-primary-600 leading-none' : 'py-2 px-4 rounded-lg',
+    className,
+  )}
   on:click={async () => {
     if (!isLoading && !isJobLoading && !disabled) {
       isJobLoading = true
