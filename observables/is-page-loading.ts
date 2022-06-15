3baa42726fb0f521d2532ac404eda293dc7796de
clonomaer <clonomaer@gmail.com>
2022-06-15 06:02:17 +0200

    add missing dep is page loading

---
+++ observables/is-page-loading.ts
@@ -0,0 +1,19 @@
+import {
+  distinctUntilChanged,
+  endWith,
+  from,
+  ignoreElements,
+  map,
+  mergeMap,
+  scan,
+  startWith,
+} from 'rxjs'
+import { pageLoadingJobs$ } from '../contexts/loading-jobs'
+
+export const isPageLoading$ = pageLoadingJobs$.pipe(
+  mergeMap(job => from(job).pipe(ignoreElements(), startWith(1), endWith(-1))),
+  scan((acc, curr) => acc + curr, 0),
+  map(x => x !== 0),
+  startWith(true),
+  distinctUntilChanged(),
+)
