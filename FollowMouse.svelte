41c6516343ab61f0a1608fd7288d9a8795690bad
clonomaer <clonomaer@gmail.com>
2022-06-15 06:00:39 +0200

    remove starting at the middle of screen for followMouse

--- FollowMouse.svelte
+++ FollowMouse.svelte
@@ -12,7 +12,7 @@
   let viewportHeight: number = 0
   let viewportWidth: number = 0
   let mousePosition: { x: number; y: number } = { x: 0, y: 0 }
-  $: mousePosition = { x: viewportWidth / 2, y: viewportHeight / 2 }
+  // $: mousePosition = { x: viewportWidth / 2, y: viewportHeight / 2 }
   let [x, setX] = useWobble({ damping: 2.5, stiffness: 3, fromValue: mousePosition.x })
   let [y, setY] = useWobble({ damping: 2.5, stiffness: 3, fromValue: mousePosition.y })
   $: {
@@ -42,7 +42,7 @@
     bind:clientWidth={width}
     style="opacity: {opacity}; transform: translate({$x - width / 2}px, {$y -
       height / 2}px); will-change: transform, opacity;"
-    class="fixed top-0 left-0 pointer-events-none select-none">
+    class="fixed z-10 top-0 left-0 pointer-events-none select-none">
     <slot />
   </div>
 {/if}
