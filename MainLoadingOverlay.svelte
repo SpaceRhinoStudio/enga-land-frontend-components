1b2096f688ef6cf9a8dea2f68e70b48d55789818
clonomaer <clonomaer@gmail.com>
2022-06-17 21:47:48 +0200

    handle loadings differently

--- MainLoadingOverlay.svelte
+++ MainLoadingOverlay.svelte
@@ -1,16 +1,23 @@
 <script>
+  import { delay } from 'rxjs'
+
   import { fade } from 'svelte/transition'
-  import IsNavigating from './IsNavigating.svelte'
   import LoadingSpinner from './LoadingSpinner.svelte'
+  import { isPageLoading$ } from './observables/is-page-loading'
+  import { Window$ } from './observables/window'
+
+  $: $isPageLoading$
+    ? Window$.subscribe(win => (win.document.body.style.overflow = 'hidden'))
+    : Window$.pipe(delay(300)).subscribe(win => (win.document.body.style.overflow = ''))
 </script>
 
-<IsNavigating>
+{#if $isPageLoading$}
   <div
-    slot="show"
-    transition:fade={{ duration: 300 }}
+    in:fade={{ duration: 150 }}
+    out:fade={{ duration: 300 }}
     style="transform: translate3d(0, 0, 0)"
     id="preloading_spinner"
     class="flex justify-center items-center fixed inset-0 z-[60] bg-primary-990">
     <LoadingSpinner --big="1" />
   </div>
-</IsNavigating>
+{/if}
---
+++ helpers/navigation-loading-job.svelte
@@ -0,0 +1,35 @@
+<script context="module" lang="ts">
+  export const intro = true
+</script>
+
+<script lang="ts">
+  import { afterNavigate, beforeNavigate, goto } from '$app/navigation'
+  import _ from 'lodash'
+  import { onMount } from 'svelte'
+  import { pageLoadingJobs$ } from '../contexts/loading-jobs'
+
+  const initialResolve = { current: _.noop }
+  pageLoadingJobs$.next(new Promise<void>(res => (initialResolve.current = res)))
+
+  const whitelist = { to: null as string | null }
+  const resolve = { current: _.noop }
+
+  beforeNavigate(({ to, cancel }) => {
+    if (!whitelist.to && to?.pathname) {
+      cancel()
+      whitelist.to = to.pathname
+      pageLoadingJobs$.next(new Promise<void>(res => (resolve.current = res)))
+      setTimeout(() => {
+        goto(whitelist.to ?? '/').then(() => (whitelist.to = null))
+      }, 250)
+    }
+  })
+
+  afterNavigate(() => {
+    setTimeout(resolve.current, 350)
+  })
+
+  onMount(() => {
+    setTimeout(initialResolve.current, 350)
+  })
+</script>
