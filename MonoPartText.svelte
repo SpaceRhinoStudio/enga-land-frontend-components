81b4c1e076fb6caac2be6121f924ef9bbfa58f5f
clonomaer <clonomaer@gmail.com>
2022-07-19 21:36:20 +0200

    add component to generate the mono containing strings html

---
+++ MonoPartText.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import cn from 'classnames'
+
+  export let text: string
+  let className: { [key in 'container' | 'normal' | 'mono']?: string } = {}
+  export { className as class }
+</script>
+
+<span class={cn(className.container)}>
+  {#each text?.split('`') ?? [] as part, i}
+    {#if i % 2 === 0}
+      <span class={cn(className.normal)}>{part}</span>
+    {:else}
+      <span class={cn(className.mono ?? 'font-mono text-text-hover')}>{part}</span>
+    {/if}
+  {/each}
+</span>
