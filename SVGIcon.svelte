3b10f191a1357f46ae3b48996ed4c8ca6e17408e
clonomaer <clonomaer@gmail.com>
2022-07-19 21:37:23 +0200

    add dimension and drawParams to SVGIcon

--- SVGIcon.svelte
+++ SVGIcon.svelte
@@ -1,31 +1,42 @@
 <script lang="ts">
   import _ from 'lodash'
   import { pxPerRem$ } from './helpers/px-rem-conversion'
-  import { draw } from 'svelte/transition' //TODO
+  import { DrawParams } from 'svelte/transition'
+  import { createEventDispatcher } from 'svelte'
+  import cn from 'classnames'
 
   export let className: string = ''
   export let style: string = ''
   export let Icon: any
-  export let width: string | undefined
-  export let height: string | undefined
-  let pxWidth: number | undefined
-  let pxHeight: number | undefined
-  $: {
-    pxWidth = _.isUndefined(width) ? undefined : parseFloat(width) * $pxPerRem$
-    pxHeight = _.isUndefined(height) ? undefined : parseFloat(height) * $pxPerRem$
-  }
+  export let width: string | undefined = undefined
+  export let height: string | undefined = undefined
+  export let dimensions: string | undefined = undefined
   export let dontFill = false
+  export let drawParams: DrawParams = {}
+
+  $: remToPx = (rem: string | undefined) =>
+    _.isUndefined(rem) ? undefined : parseFloat(rem) * $pxPerRem$
+
+  $: pxWidth = remToPx(width)
+  $: pxHeight = remToPx(height)
+  $: pxDimensions = remToPx(dimensions)
+
+  const dispatch = createEventDispatcher<{ click: MouseEvent }>()
 </script>
 
-<div class={`flex items-center justify-center ${className}`} {style}>
+<div
+  class={`flex items-center justify-center ${className}`}
+  {style}
+  on:click={e => dispatch('click', e)}>
   <svelte:component
     this={Icon}
-    width={pxWidth}
-    height={pxHeight}
-    class={`
-        ${!height ? 'h-full' : ''}
-        ${!width ? 'w-full' : ''}
-        ${!dontFill ? 'fillSVG' : ''}
-        ${!dontFill ? 'fillStroke' : ''}
-    `} />
+    width={pxWidth ?? pxDimensions}
+    height={pxHeight ?? pxDimensions}
+    class={cn(
+      !(height ?? dimensions) && 'h-full',
+      !(width ?? dimensions) && 'w-full',
+      !dontFill && 'fillSVG',
+      !dontFill && 'fillStroke',
+    )}
+    {drawParams} />
 </div>
