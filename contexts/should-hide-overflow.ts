d6c480e235a188256308e2fd31ae2d95f24955c5
clonomaer <clonomaer@gmail.com>
2022-07-19 21:19:15 +0200

    add feature to use overflow controller with a specified id

--- contexts/should-hide-overflow.ts
+++ contexts/should-hide-overflow.ts
@@ -1,12 +1,27 @@
-import { delay, map, mergeMap, of, scan, Subject, withLatestFrom } from 'rxjs'
+import { delay, map, mergeMap, of, scan, Subject, withLatestFrom, EMPTY, tap } from 'rxjs'
 import { controlStreamPayload } from '../operators/control-stream-payload'
 import { Window$ } from '../observables/window'
+import _ from 'lodash'
 
-export const shouldHideOverflowController$ = new Subject<{ Hide?: boolean }>()
+export const shouldHideOverflowController$ = new Subject<{
+  Hide?: boolean | { id: string; hide: boolean }
+}>()
+
+const hides: Record<string, boolean> = {}
 
 export const shouldHideOverflow$ = shouldHideOverflowController$.pipe(
   controlStreamPayload('Hide'),
-  mergeMap(x => (x ? of(1) : of(-1).pipe(delay(300)))),
+  mergeMap(x => {
+    let res = _.isBoolean(x) ? x : null
+    if (!_.isBoolean(x)) {
+      res = x.hide && !hides[x.id] ? true : !x.hide && hides[x.id] ? false : null
+      if (_.isBoolean(res)) {
+        //side effect
+        hides[x.id] = res
+      }
+    }
+    return res === true ? of(1) : res === false ? of(-1).pipe(delay(300)) : EMPTY
+  }),
   scan((acc, x) => acc + x, 0),
   map(x => !!x),
 )
