60f41111c0916ef996b977b322d22573361abb84
clonomaer <clonomaer@gmail.com>
2022-08-11 07:18:59 +0200

    add error page

---
+++ ErrorPage.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { __$ } from './locales'
+
+  export let message: string
+  export let status: string | number
+</script>
+
+<div class="flex flex-col grow gap-4 items-center justify-center">
+  <span class="text-4xl font-serif text-text-hover tracking-wide">
+    {status == 404 ? $__$.nav.notFound : $__$.main.genericErrorMessage}
+  </span>
+  <span class="text-text-secondary">{status}</span>
+</div>
