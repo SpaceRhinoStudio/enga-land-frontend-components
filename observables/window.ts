3bf0b95d3ec9fc87ac97196ce12b21573c8b9422
clonomaer <clonomaer@gmail.com>
2022-08-11 08:36:33 +0200

    add online status and window focus status

--- observables/window.ts
+++ observables/window.ts
@@ -2,10 +2,55 @@
  * @description this observable is used to make sure that client side related code is not executed on server
  */
 
-import { EMPTY, iif, of, map } from 'rxjs'
+import {
+  EMPTY,
+  iif,
+  of,
+  map,
+  shareReplay,
+  switchMap,
+  interval,
+  distinctUntilChanged,
+  skip,
+  withLatestFrom,
+  filter,
+  merge,
+  fromEvent,
+} from 'rxjs'
+import { flashToast$, ToastLevel } from '../contexts/flash-toast'
+import { __$ } from '../locales'
 
 export const Window$ = iif(
   () => typeof window !== 'undefined',
   of(null).pipe(map(() => window)),
   EMPTY,
+)
+
+export const onlineStatus$ = Window$.pipe(
+  switchMap(win => interval(1000).pipe(map(() => win.navigator.onLine))),
+  distinctUntilChanged(),
+  shareReplay(1),
+)
+
+onlineStatus$
+  .pipe(
+    filter((x, i) => (i === 0 ? !x : true)),
+    withLatestFrom(__$),
+  )
+  .subscribe(([status, __]) => {
+    flashToast$.next({
+      message: __.main.onlineStatus[status ? 'online' : 'offline'],
+      level: status ? ToastLevel.SUCCESS : ToastLevel.ERROR,
+      timeout: status ? 2000 : 10000,
+    })
+  })
+
+export const windowFocus$ = Window$.pipe(
+  switchMap(win =>
+    merge(
+      fromEvent(win, 'focus').pipe(map(() => true)),
+      fromEvent(win, 'blur').pipe(map(() => false)),
+    ),
+  ),
+  shareReplay(1),
 )
