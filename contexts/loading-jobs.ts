1585825b35fcf085ec03dd2cd7aebe1fc7a034d0
clonomaer <clonomaer@gmail.com>
2022-08-11 08:34:33 +0200

    nit

--- contexts/loading-jobs.ts
+++ contexts/loading-jobs.ts
@@ -2,8 +2,8 @@
  * @description a queue of jobs that are eligible to summon the main loading overlay that blocks the user from interacting with the page
  */
 
-import { BehaviorSubject, Observable } from 'rxjs'
+import { BehaviorSubject, Observable, of } from 'rxjs'
 
 export const pageLoadingJobs$ = new BehaviorSubject<Promise<unknown> | Observable<unknown>>(
-  Promise.resolve(undefined),
+  of(undefined),
 )
